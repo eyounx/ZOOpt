a5fd1fd2a241e4a681cd34020bb51518a4e38759
liu <141242026@smail.nju.edu.cn>
2017-04-16 21:46:38 +0800

    Debug

--- example/sparse_regression/test.py
+++ example/sparse_regression/test.py
@@ -1,5 +1,3 @@
-from zoo.algos.paretoopt.ParetoOptimization import ParetoOptimization
-#from zoo.algos.racos.racos_optimization import RacosOptimization
 import numpy as np
 from zoo.algos.paretoopt.NormalizeData import NormlizeDate
 
@@ -7,16 +5,16 @@ from zoo.opt import Opt
 from zoo.parameter import Parameter
 from zoo.objective import Objective
 
-if __name__=='__main__':
-    opt=Opt()
-    parameter=Parameter()
-    objective=Objective()
-    orginX=NormlizeDate("housing.txt")
+if __name__ == '__main__':
+    opt = Opt()
+    parameter = Parameter()
+    objective = Objective()
+    orginX = NormlizeDate("housing.txt")
     n=np.shape(orginX)[1]
-    X=orginX[:,0:n-1]
-    y=orginX[:,n-1]
-    parameter.set_paretoopt_parameters(X,y,8)
+    X = orginX[:, 0:n-1]
+    y = orginX[:, n-1]
+    parameter.set_paretoopt_parameters(X, y, 8)
     parameter.set_algorithm('poss')
     objective.set_constraint(True)
-    result=opt.min(objective, parameter)
+    result = opt.min(objective, parameter)
     print result
