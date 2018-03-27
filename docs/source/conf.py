e92a2f1ff51bdef1f39611409931bc53ef66b770
Yu-Ren Liu <liuyr@lamda.nju.edu.cn>
2018-03-27 10:22:40 +0800

    update building docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,7 +63,7 @@ source_suffix = '.rst'
 # source_encoding = 'utf-8-sig'
 
 # The master toctree document.
-master_doc = 'zoopt'
+master_doc = 'index'
 
 # General information about the project.
 project = u'ZOOpt'
