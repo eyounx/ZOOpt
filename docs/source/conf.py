896e91ca531471e3c8489834374b2b58b4f08245
Yu-Ren Liu <liuyr@lamda.nju.edu.cn>
2018-03-27 10:42:08 +0800

    add markdown files to readthedocs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -55,8 +55,12 @@ templates_path = ['_templates']
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
 #
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_parsers = {
+    '.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+# source_suffix = '.rst'
 
 # The encoding of source files.
 #
